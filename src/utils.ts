0dbb9bdd2ec24cc5d7afd141776a7622196e5e51
Anselme Habumugisha Irumva <61510549+irumvanselme@users.noreply.github.com>
2023-02-16 12:41:29 +0200

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 export const proxyConf: Record<string, string> = {
   "baza-apis": "http://69.28.91.84:8001",
-  "safaribus-test-app": "http://197.243.20.77:8080"
+  "safaribus-test-app": "http://197.243.20.77:8080",
+  "safaribus-finance-url": "http://197.243.20.77"
 }
